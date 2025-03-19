6d37912896805549ab60f3fb9c9c29473534507b
Andree-G <gammella.andrea@itisgrassi.edu.it>
2025-03-19 10:21:19 +0100

    -

---
+++ Ascensore.java
@@ -0,0 +1,17 @@
+public class Ascensore
+{
+  private int pianocorrente;
+  private int capienzamassima;
+  private boolean porteAperte;
+  
+  private ArrayList<Persona> personeDentro; 
+
+  public Ascensore(int pianocorrente, int capienzamassima);
+  {
+    this.pianocorrente = pianocorrente;
+    this.capienzamassima = capienzamassima;
+    this.porte = porte;
+    personeDentro = new ArrayList<>;
+  }
+
+}
